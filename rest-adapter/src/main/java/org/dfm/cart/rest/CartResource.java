7cb33b0338995f8da8663ccb8c3b57eeba86a4b2
Paul Williams <paul58914080@gmail.com>
2021-02-25 10:39:54 +0530

    chore: added cross origin

--- rest-adapter/src/main/java/org/dfm/cart/rest/CartResource.java
+++ rest-adapter/src/main/java/org/dfm/cart/rest/CartResource.java
@@ -1,6 +1,7 @@
 package org.dfm.cart.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.dfm.cart.domain.model.Cart;
 import org.dfm.cart.domain.model.CartInfo;
 import org.dfm.cart.domain.port.RequestCart;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/carts")
 public class CartResource {
